6cebf8484c1279a6058028a65199a1aa4614f82b
Krystian <krystiano1602@gmail.com>
2025-02-14 14:03:56 +0100

    Add .gitignore file to frontend

---
+++ OptiSched/OptiSched/Migrations/20250113211746_addScheduleListToRoom.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OptiSched.Migrations
+{
+    /// <inheritdoc />
+    public partial class addScheduleListToRoom : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Schedules_RoomId",
+                table: "Schedules");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Schedules_RoomId",
+                table: "Schedules",
+                column: "RoomId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Schedules_RoomId",
+                table: "Schedules");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Schedules_RoomId",
+                table: "Schedules",
+                column: "RoomId",
+                unique: true);
+        }
+    }
+}
